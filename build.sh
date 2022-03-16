9ff5f68713281bddace8226c2fbe19dfef5255c2
Jan Vraštil <vrastil.honza@seznam.cz>
2022-03-16 22:23:43 +0100

    force production build

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-if [ "$CI_GIT_REF" = "master" ]; then
-        npm run build:hotfix
-        # case "$CI_PLATFORM" in
-        # "android") npm run build:ng:prod
-        # ;;
-        # "ios") npm run build:ios:prod
-        # ;;
-        # esac
-    else
-        npm run build:dev
-        # case "$CI_PLATFORM" in
-        # "android") npm run build:ng
-        # ;;
-        # "ios") npm run build:ios
-        # ;;
-        # esac
-fi
+#if [ "$CI_GIT_REF" = "master" ]; then
+#        npm run build:hotfix
+         case "$CI_PLATFORM" in
+         "android") npm run build:ng:prod
+         ;;
+         "ios") npm run build:ios:prod
+         ;;
+         esac
+#    else
+#        npm run build:dev
+#        # case "$CI_PLATFORM" in
+#        # "android") npm run build:ng
+#        # ;;
+#        # "ios") npm run build:ios
+#        # ;;
+#        # esac
+#fi
