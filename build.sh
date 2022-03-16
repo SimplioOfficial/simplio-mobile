32b6dbf4d6f11ef0f26131e4170d01100772b788
Filip Storkan <filip.storkan@gmail.com>
2022-03-16 08:43:54 -0300

    updated build script

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 if [ "$CI_GIT_REF" = "master" ]; then
-        case "$CI_PLATFORM" in
-        "android") npm run build:ng:prod
-        ;;
-        "ios") npm run build:ios:prod
-        ;;
-        esac
+        npm run build:hotfix
+        # case "$CI_PLATFORM" in
+        # "android") npm run build:ng:prod
+        # ;;
+        # "ios") npm run build:ios:prod
+        # ;;
+        # esac
     else
         npm run build:dev
         # case "$CI_PLATFORM" in
