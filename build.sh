a7290d92fcb6002b58de458496a435441bd82663
Jan Vraštil <vrastil.honza@seznam.cz>
2022-03-16 17:57:29 +0100

    Revert "updated build script"
    This reverts commit 32b6dbf4d6f11ef0f26131e4170d01100772b788.

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 if [ "$CI_GIT_REF" = "master" ]; then
-        npm run build:hotfix
-        # case "$CI_PLATFORM" in
-        # "android") npm run build:ng:prod
-        # ;;
-        # "ios") npm run build:ios:prod
-        # ;;
-        # esac
+        case "$CI_PLATFORM" in
+        "android") npm run build:ng:prod
+        ;;
+        "ios") npm run build:ios:prod
+        ;;
+        esac
     else
         npm run build:dev
         # case "$CI_PLATFORM" in
